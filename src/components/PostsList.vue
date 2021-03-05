c299f3ef0d519eb517abd2b4f6f554b83d508681
minhduc1612112 <duc1612112@gmail.com>
2021-03-05 11:32:06 +0700

    First commit

---
+++ src/components/PostsList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="post-list">
+      <div v-for="post in posts" :key="post.id">
+          <single-post :post="post"></single-post>
+      </div>
+  </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from 'vue';
+import SinglePost from './SinglePost';
+export default {
+    name: 'PostsList',
+    components: {
+        SinglePost,
+    },
+    props: ['posts'],
+    setup(props) {
+        onMounted(() => {
+            console.log('Component monuted');
+        });
+        onUnmounted(() => {
+            console.log('Component unmounted');
+        });
+        onUpdated(() => {
+            console.log('Component updated');
+        })
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <p>Home</p>
+    <posts-list :posts="posts"></posts-list>
+  </div>
+</template>
+
+<script>
+import { ref, reactive, computed, watch, watchEffect } from 'vue';
+import PostsList from '../components/PostsList';
+export default {
+  name: 'Home',
+  components: {
+    PostsList,
+  },
+  setup() {
+    const posts = ref([{
+      title: 'Welcome to the blog',
+      body: 'Lorem ipsum',
+      id: 1
+    }, {
+      title: 'Top 5 CSS tips',
+      body: 'Lorem ipsum',
+      id: 2
+    }]);
+
+    return {
+      posts,
+    }
+  }
+}
+</script>
